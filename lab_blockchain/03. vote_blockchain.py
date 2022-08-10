0487e204b8ac5a89e2f9c80aff45cd734359274d
박정현 <pjh9996@naver.com>
2022-08-10 22:00:18 +0900

    Create 03. vote_blockchain.py

---
+++ lab_blockchain/03. vote_blockchain.py
@@ -0,0 +1,22 @@
+chain = []
+
+block1 = {
+    'type': 'open',
+    'data': {
+        'id': '투표 ID',
+        'question': '투표 질문',
+        'options': ['투표 항목1', '투표 항목2', '투표 항목3']
+    }
+}
+chain.append(block1)
+print(chain)
+
+block2 = {
+    'type': 'vote',
+    'data': {
+        'id': '투표 ID',
+        'vote': '투표 항목1'
+    }
+}
+chain.append(block2)
+print(chain)
