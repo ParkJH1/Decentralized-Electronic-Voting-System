6bdd99bf4a8ec9ed61054d8504ec8bfaa60f82cd
박정현 <pjh9996@naver.com>
2022-09-03 17:57:25 +0900

    Update 03.py

--- quiz/03.py
+++ quiz/03.py
@@ -6,6 +6,57 @@ class Tab1(QWidget):
     def __init__(self):
         super().__init__()
 
+        #####################################################
+        self.menu_group_box = QGroupBox('메뉴')
+
+        self.fetch_vote_button = QPushButton('투표 조회')
+
+        self.menu_hbox_layout = QHBoxLayout()
+        self.menu_hbox_layout.addWidget(self.fetch_vote_button)
+
+        self.menu_group_box.setLayout(self.menu_hbox_layout)
+        #####################################################
+
+        #####################################################
+        self.vote_list_group_box = QGroupBox('투표 목록')
+
+        self.vote_list = QListWidget()
+        self.vote_list.addItem('투표 1')
+        self.vote_list.addItem('투표 2')
+
+        self.vote_list_vbox_layout = QVBoxLayout()
+        self.vote_list_vbox_layout.addWidget(self.vote_list)
+
+        self.vote_list_group_box.setLayout(self.vote_list_vbox_layout)
+        #####################################################
+
+        #####################################################
+        self.vote_info_group_box = QGroupBox('투표 정보')
+
+        self.question_label = QLabel()
+        self.question_label.setText('투표 질문')
+
+        self.option1_button = QPushButton('선택지 1')
+        self.option2_button = QPushButton('선택지 2')
+        self.option3_button = QPushButton('선택지 3')
+
+        self.vote_info_vbox_layout = QVBoxLayout()
+        self.vote_info_vbox_layout.addWidget(self.question_label)
+        self.vote_info_vbox_layout.addWidget(self.option1_button)
+        self.vote_info_vbox_layout.addWidget(self.option2_button)
+        self.vote_info_vbox_layout.addWidget(self.option3_button)
+
+        self.vote_info_group_box.setLayout(self.vote_info_vbox_layout)
+        #####################################################
+
+        self.vote_layout = QGridLayout()
+        self.vote_layout.addWidget(self.menu_group_box, 0, 0, 1, 2)
+        self.vote_layout.addWidget(self.vote_list_group_box, 1, 0, 1, 1)
+        self.vote_layout.addWidget(self.vote_info_group_box, 1, 1, 1, 1)
+
+        self.setLayout(self.vote_layout)
+
+
 
 class Tab2(QWidget):
     def __init__(self):
