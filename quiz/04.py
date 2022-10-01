37b8b20d69f3f09c7291386f7d0bef7d4625a694
박정현 <pjh9996@naver.com>
2022-10-01 16:34:26 +0900

    Create 04.py

---
+++ quiz/04.py
@@ -0,0 +1,36 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+import sys
+
+import json
+import socket
+import uuid
+
+
+class Tab1(QWidget):
+    def __init__(self, devs):
+        super().__init__()
+        self.devs = devs
+        self.current_vote_id = -1
+
+        self.vote_list_group_box = QGroupBox('투표 목록')
+        self.vote_list = dict()
+        self.vote_list_widget = QListWidget()
+        self.vote_list_widget.clicked.connect(self.select_vote)
+        self.vote_list_layout = QVBoxLayout()
+        self.vote_list_layout.addWidget(self.vote_list_widget)
+        self.vote_list_group_box.setLayout(self.vote_list_layout)
+
+        self.vote_group_box = QGroupBox('투표')
+        self.question_label = QLabel(self)
+        self.option1_button = QPushButton()
+        self.option2_button = QPushButton()
+        self.option3_button = QPushButton()
+        self.vote_layout = QVBoxLayout()
+        self.vote_layout.addWidget(self.question_label)
+        self.vote_layout.addWidget(self.option1_button)
+        self.vote_layout.addWidget(self.option2_button)
+        self.vote_layout.addWidget(self.option3_button)
+        self.vote_group_box.setLayout(self.vote_layout)
+
