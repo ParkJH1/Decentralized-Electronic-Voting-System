e88c2dbc7e265b990d70f86949b8fb6e1c7fa0b8
박정현 <pjh9996@naver.com>
2022-10-01 16:59:17 +0900

    Update 04.py

--- quiz/04.py
+++ quiz/04.py
@@ -51,6 +51,26 @@ class Tab1(QWidget):
 
         self.setLayout(self.main_layout)
 
+        self.update_vote_list()
+
+    def update_vote_list(self):
+        self.vote_list.clear()
+        self.vote_list_widget.clear()
+        for block in self.devs.chain:
+            if block['type'] == 'open':
+                vote_id = block['data']['id']
+                self.vote_list_widget.addItem(vote_id)
+                self.vote_list[vote_id] = block['data']
+                self.vote_list[vote_id]['total_vote'] = 0
+                self.vote_list[vote_id]['vote_count'] = dict()
+                for option in block['data']['options']:
+                    self.vote_list[vote_id]['vote_count'][option] = 0
+            elif block['type'] == 'vote':
+                vote_id = block['data']['id']
+                self.vote_list[vote_id]['total_vote'] += 1
+                self.vote_list[vote_id]['vote_count'][block['data']['vote']] += 1
+        self.update_vote()
+
 
 def exception_hook(except_type, value, traceback):
     print(except_type, value, traceback)
