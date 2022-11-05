027258342176ed8e89d4625f695a71d0befccbdc
박정현 <pjh9996@naver.com>
2022-11-05 15:51:55 +0900

    Update 06.py

--- quiz/06.py
+++ quiz/06.py
@@ -22,3 +22,15 @@ def get_block_hash(block):
     return hashlib.sha256(str(data).encode()).hexdigest()
 
 
+def get_block_signature(block, key):
+    data = dict()
+    data['type'] = block['transaction']['type']
+    data['data'] = sorted(block['transaction']['data'].copy().items())
+    data['author'] = block['author']
+    data['previous_hash'] = block['previous_hash']
+    data = sorted(data.items())
+    signature = key.sign(str(data).encode())
+    return base64.b64encode(signature).decode()
+
+
+
