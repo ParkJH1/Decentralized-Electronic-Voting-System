803259da9f9eebf02209f395a89425a8f38d5308
박정현 <pjh9996@naver.com>
2022-11-05 15:43:22 +0900

    Create 06.py

---
+++ quiz/06.py
@@ -0,0 +1,24 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+import os
+import sys
+
+import json
+import socket
+import uuid
+from ecdsa import SigningKey, VerifyingKey
+import hashlib
+import base64
+
+
+def get_block_hash(block):
+    data = dict()
+    data['type'] = block['transaction']['type']
+    data['data'] = sorted(block['transaction']['data'].copy().items())
+    data['author'] = block['author']
+    data['previous_hash'] = block['previous_hash']
+    data = sorted(data.items())
+    return hashlib.sha256(str(data).encode()).hexdigest()
+
+
